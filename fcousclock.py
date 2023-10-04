dd177e720b6a07c3acde9e711625a52c2e5b3411
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-05 02:06:09 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,21 +1,21 @@
 import time
-import sys
 
-def countdown(minutes):
+def focus_timer(minutes):
     seconds = minutes * 60
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timeformat = f"{mins:02d}:{secs:02d}"
-        sys.stdout.write(f"剩余时间: {timeformat}\r")
-        sys.stdout.flush()
+    start_time = time.time()
+    end_time = start_time + seconds
+
+    print(f"Focus timer started for {minutes} minutes.")
+
+    while time.time() < end_time:
+        remaining_time = int(end_time - time.time())
+        minutes_left = remaining_time // 60
+        seconds_left = remaining_time % 60
+        time_left = f"{minutes_left:02d}:{seconds_left:02d}"
+        print(f"Time left: {time_left}", end="\r")
         time.sleep(1)
-        seconds -= 1
 
-    print("\n时间到！专注时间已结束。")
+    print("\nFocus timer completed!")
 
-if __name__ == "__main__":
-    try:
-        minutes = int(input("请输入专注时间（分钟）："))
-        countdown(minutes)
-    except ValueError:
-        print("无效的输入。请输入一个整数分钟数。")
+# 设置专注时长为25分钟
+focus_timer(25)

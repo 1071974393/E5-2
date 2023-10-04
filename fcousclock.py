05245f318a0614e214af95c681eff7bb1c60ac03
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-05 02:07:28 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -5,17 +5,17 @@ def focus_timer(minutes):
     start_time = time.time()
     end_time = start_time + seconds
 
-    print(f"Focus timer started for {minutes} minutes.")
+    print(f"专注时钟开始，时长 {minutes} 分钟。")
 
     while time.time() < end_time:
         remaining_time = int(end_time - time.time())
         minutes_left = remaining_time // 60
         seconds_left = remaining_time % 60
         time_left = f"{minutes_left:02d}:{seconds_left:02d}"
-        print(f"Time left: {time_left}", end="\r")
+        print(f"剩余时间：{time_left}", end="\r")
         time.sleep(1)
 
-    print("\nFocus timer completed!")
+    print("\n专注时钟结束！")
 
 # 设置专注时长为25分钟
 focus_timer(25)

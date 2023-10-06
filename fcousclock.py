e6d2bc41918330e1108d9d452d3425b51c071346
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-06 23:13:33 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,21 +1,20 @@
 import time
+import winsound
 
 def focus_timer(minutes):
     seconds = minutes * 60
-    start_time = time.time()
-    end_time = start_time + seconds
-
-    print(f"专注时钟开始，时长 {minutes} 分钟。")
-
-    while time.time() < end_time:
-        remaining_time = int(end_time - time.time())
-        minutes_left = remaining_time // 60
-        seconds_left = remaining_time % 60
-        time_left = f"{minutes_left:02d}:{seconds_left:02d}"
-        print(f"剩余时间：{time_left}", end="\r")
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')
         time.sleep(1)
+        seconds -= 1
 
-    print("\n专注时钟结束！")
+    # 专注时间结束后的提醒音
+    frequency = 1500  # 提醒音频率（以赫兹为单位）
+    duration = 2000  # 提醒音持续时间（以毫秒为单位）
+    winsound.Beep(frequency, duration)
+    print("时间到！专注时间结束。")
 
-# 设置专注时长为25分钟
+# 设置专注时间为25分钟
 focus_timer(25)

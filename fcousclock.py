3ceaa34ebf8f94c18c4f823e691ec191109aeaba
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-06 23:14:08 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,14 +1,39 @@
+import tkinter as tk
 import time
 
-def focus_timer(minutes):
-    seconds = minutes * 60
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        seconds -= 1
-    print("时间到！专注时间结束。")
+# 创建一个窗口
+window = tk.Tk()
+window.title("专注时钟")
 
-# 设置专注时间为25分钟
-focus_timer(25)
+# 设置初始时间（以秒为单位）
+remaining_time = 25 * 60  # 25分钟
+
+# 显示剩余时间的标签
+time_label = tk.Label(window, text="", font=("Helvetica", 48))
+time_label.pack()
+
+# 更新剩余时间的函数
+def update_time():
+    global remaining_time
+    if remaining_time > 0:
+        minutes = remaining_time // 60
+        seconds = remaining_time % 60
+        time_label.config(text=f"{minutes:02d}:{seconds:02d}")
+        remaining_time -= 1
+        window.after(1000, update_time)  # 每秒更新一次
+    else:
+        time_label.config(text="时间到！")
+
+# 开始计时
+def start_timer():
+    global remaining_time
+    if remaining_time > 0:
+        start_button.config(state=tk.DISABLED)
+        update_time()
+
+# 创建开始按钮
+start_button = tk.Button(window, text="开始", command=start_timer)
+start_button.pack()
+
+# 运行窗口
+window.mainloop()

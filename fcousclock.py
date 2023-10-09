b3fe85627082120266e8a052c6e180c02930cae6
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-09 19:20:52 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,21 +1,12 @@
 import time
-import winsound
 
-def start_focus_timer(duration_in_minutes):
-    # 将输入的分钟数转换为秒数
-    duration_in_seconds = duration_in_minutes * 60
+def focus_timer(duration):
+    print("开始专注...")
+    time.sleep(duration * 60)
+    print("专注时间结束！")
 
-    print("开始专注，持续 {} 分钟...".format(duration_in_minutes))
+# 设置专注时长（以分钟为单位）
+focus_duration = 25
 
-    # 等待指定时间
-    time.sleep(duration_in_seconds)
-
-    # 播放提示音
-    frequency = 1500  # 声音频率
-    duration = 1000   # 声音持续时间
-    winsound.Beep(frequency, duration)
-
-    print("专注时间到！")
-
-# 以25分钟为一段时间进行专注
-start_focus_timer(25)
+# 启动专注时钟
+focus_timer(focus_duration)

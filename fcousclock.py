718c02f8ec176eb0115eae117a1dbd8717b89723
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-13 18:32:57 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,14 +1,16 @@
 import time
 
-def focus_timer(duration):
-    print("专注时钟开始")
+def focus_timer(duration, task_name):
+    print(f"{task_name}专注时钟开始")
     for remaining in range(duration, 0, -1):
         minutes, seconds = divmod(remaining, 60)
         timeformat = f"{minutes:02d}:{seconds:02d}"
         print(timeformat, end='\r')
         time.sleep(1)
-    print("专注时钟结束")
+    print(f"{task_name}专注时钟结束")
 
 if __name__ == "__main__":
-    focus_duration = 25 * 60  # 25分钟，可以根据需要调整
-    focus_timer(focus_duration)
+    work_duration = 25 * 60  # 25分钟工作时长
+    work_task_name = "工作"  # 任务名称
+    focus_timer(work_duration, work_task_name)
+

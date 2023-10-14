86df51fc7253f2ed8cb8bbc63c87429d3c228d26
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-15 00:41:48 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,26 +1,26 @@
 import time
+import threading
 
-def focus_timer(focus_minutes, break_minutes):
-    focus_seconds = focus_minutes * 60
-    break_seconds = break_minutes * 60
-
-    while focus_seconds:
-        mins, secs = divmod(focus_seconds, 60)
-        timeformat = 'Focus: {:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
+def focus_timer(minutes, timer_id):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(f"Timer {timer_id}: {timeformat}", end='\r')
         time.sleep(1)
-        focus_seconds -= 1
-
-    print("专注时间结束！")
-    
-    while break_seconds:
-        mins, secs = divmod(break_seconds, 60)
-        timeformat = 'Break: {:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        break_seconds -= 1
+        seconds -= 1
+    print(f"Timer {timer_id} 完成！")
 
 if __name__ == "__main__":
+    timers = []
     focus_minutes = 25  # 设置专注时间为25分钟
-    break_minutes = 5   # 设置休息时间为5分钟
-    focus_timer(focus_minutes, break_minutes)
+
+    for i in range(1, 5):  # 创建4个专注时钟
+        timer = threading.Thread(target=focus_timer, args=(focus_minutes, i))
+        timers.append(timer)
+        timer.start()
+
+    for timer in timers:
+        timer.join()
+
+    print("所有专注时钟完成！")

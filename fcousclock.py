7b8a3cf19d1385d4179b76c627e0a700a51ed9f6
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-17 19:30:36 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -3,9 +3,7 @@ import time
 def countdown(minutes):
     seconds = minutes * 60
     while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        print(f"Remaining Time: {seconds // 60} minutes {seconds % 60} seconds", end="\r")
         time.sleep(1)
         seconds -= 1
 

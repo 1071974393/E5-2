7002d4cf2bc4effae2b3207d11b6afa4d910efe5
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-17 19:28:24 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,26 +1,22 @@
-import time
-import threading
+import tkinter as tk
 
-def focus_timer(minutes, timer_id):
-    seconds = minutes * 60
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(f"Timer {timer_id}: {timeformat}", end='\r')
-        time.sleep(1)
-        seconds -= 1
-    print(f"Timer {timer_id} 完成！")
+def countdown():
+    global time_remaining
+    if time_remaining <= 0:
+        root.destroy()
+        # 在时间结束后可以执行一些操作，比如播放音乐或者打开其他的应用
+    else:
+        mins, secs = divmod(time_remaining, 60)
+        timer_label.config(text='{:02d}:{:02d}'.format(mins, secs))
+        time_remaining -= 1
+        root.after(1000, countdown)
 
-if __name__ == "__main__":
-    timers = []
-    focus_minutes = 25  # 设置专注时间为25分钟
+root = tk.Tk()
+root.title('专注时钟')
+time_remaining = 25 * 60  # 设置初始时间为25分钟
+timer_label = tk.Label(root, text='25:00', font=('Helvetica', 48))
+timer_label.pack(pady=20)
+countdown_button = tk.Button(root, text='开始计时', command=countdown)
+countdown_button.pack()
+root.mainloop()
 
-    for i in range(1, 5):  # 创建4个专注时钟
-        timer = threading.Thread(target=focus_timer, args=(focus_minutes, i))
-        timers.append(timer)
-        timer.start()
-
-    for timer in timers:
-        timer.join()
-
-    print("所有专注时钟完成！")

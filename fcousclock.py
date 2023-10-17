8bbd3c36dc6755fc405223e4ab3ada3829d66c9c
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-18 01:17:28 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,13 +1,14 @@
 import time
 
-def countdown(minutes):
+def focus_timer(minutes):
     seconds = minutes * 60
     while seconds > 0:
-        print(f"Remaining Time: {seconds // 60} minutes {seconds % 60} seconds", end="\r")
+        mins, secs = divmod(seconds, 60)
+        timer = "{:02d}:{:02d}".format(mins, secs)
+        print(f"Remaining time: {timer}", end="\r")
         time.sleep(1)
         seconds -= 1
+    print("Time's up! Stay focused!")
 
-    print("Time's up!")
-
-work_time = 25  # 设置工作时间为25分钟
-countdown(work_time)
+# 设置专注时长为25分钟
+focus_timer(25)

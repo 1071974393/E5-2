6ed0a513bcceb2df7f1c509c15c6838eea272a8c
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-19 19:08:41 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,19 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox
 import time
 
-def focus_timer(minutes):
-    seconds = minutes * 60
-    while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print("专注倒计时: " + timeformat, end='\r')
-        time.sleep(1)
-        seconds -= 1
-
-    print("\n专注时间结束！")
-
-if __name__ == "__main__":
+def start_timer():
     try:
-        focus_minutes = int(input("请输入专注的分钟数："))
-        focus_timer(focus_minutes)
+        minutes = int(entry.get())
+        seconds = minutes * 60
+        while seconds:
+            mins, secs = divmod(seconds, 60)
+            timer_label.config(text=f'{mins:02d}:{secs:02d}')
+            window.update()
+            time.sleep(1)
+            seconds -= 1
+        messagebox.showinfo("专注时间结束", "专注时间已结束！")
     except ValueError:
-        print("无效的输入。请输入一个整数分钟数。")
+        messagebox.showerror("错误", "请输入有效的分钟数")
+
+window = tk.Tk()
+window.title("专注时钟")
+
+instruction_label = tk.Label(window, text="请输入专注的分钟数：")
+instruction_label.pack(pady=10)
+
+entry = tk.Entry(window)
+entry.pack()
+
+start_button = tk.Button(window, text="开始专注", command=start_timer)
+start_button.pack(pady=10)
+
+timer_label = tk.Label(window, text="", font=("Helvetica", 48))
+timer_label.pack()
+
+window.mainloop()

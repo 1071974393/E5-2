a458f96b5a8aa7365371945bb4471b96e5b8700a
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-19 19:06:08 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,16 +1,11 @@
 import time
 
-def focus_timer(total_time, break_time):
-    while total_time > 0:
-        print(f"专注时间剩余：{total_time} 秒")
-        time.sleep(1)
-        total_time -= 1
-        if total_time == 0:
-            print("休息时间！")
-            time.sleep(break_time)
-            total_time = int(input("请输入下一轮专注时间（秒）："))
+def focus_timer(duration_in_minutes):
+    duration_in_seconds = duration_in_minutes * 60
+    print(f"专注时间开始，持续 {duration_in_minutes} 分钟.")
+    time.sleep(duration_in_seconds)
+    print("专注时间结束，可以休息一下！")
 
 if __name__ == "__main__":
-    total_time = int(input("请输入专注时间（秒）："))
-    break_time = int(input("请输入休息时间（秒）："))
-    focus_timer(total_time, break_time)
+    focus_duration = int(input("请输入专注时间（分钟）: "))
+    focus_timer(focus_duration)

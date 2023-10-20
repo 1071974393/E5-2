10f4c942e8c37892e9caa38d1c5ec723f5c80cf4
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-20 17:37:51 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,16 +1,15 @@
 import time
 
-def focus_timer(minutes):
-    seconds = minutes * 60
-    while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timeformat = f"{mins:02d}:{secs:02d}"
-        print(f"专注倒计时: {timeformat}", end='\r')
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
         time.sleep(1)
-        seconds -= 1
+        t -= 1
 
-    print("\n专注时间结束！")
+    print('Break Time!')
+    # 循环 5 分钟休息计时器
+    countdown(5*60)
 
-if __name__ == "__main__":
-    focus_minutes = 30
-    focus_timer(focus_minutes)
+countdown(25*60)

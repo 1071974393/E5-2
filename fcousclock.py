e2f592a2dacec817fe1414f721a960e6f91ea6e9
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-10-20 17:40:06 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,25 +1,13 @@
 import time
 
-def focus_timer(minutes):
+def pomodoro_timer(minutes):
     seconds = minutes * 60
     while seconds > 0:
-        # 计算剩余时间的小时、分钟、秒数
-        hours, remainder = divmod(seconds, 3600)
-        mins, secs = divmod(remainder, 60)
-        
-        # 格式化时间显示
-        timer = "{:02d}:{:02d}:{:02d}".format(hours, mins, secs)
-        
-        # 输出剩余时间并更新
-        print("Remaining time: {}".format(timer), end="\r")
-        
-        # 等待1秒
+        print(f"Remaining time: {seconds // 60} minutes {seconds % 60} seconds")
         time.sleep(1)
-        
-        # 减去1秒的时间
         seconds -= 1
-    
-    print("Time's up! Stay focused!")
+    print("Time's up!")
 
-# 设置专注时长为45分钟
-focus_timer(45)
+pomodoro_timer(25)
+
+

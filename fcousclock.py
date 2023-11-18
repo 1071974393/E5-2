09f0a36d436210860070ae6ceb94354b18425be8
1071974393 <140645220+1071974393@users.noreply.github.com>
2023-11-18 22:33:12 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,14 +1,36 @@
-import time
+下面是另一个专注时钟的Python指令，使用了tkinter图形界面库：
 
-def focus_timer(minutes):
+```python
+import tkinter as tk
+
+
+def start_timer(minutes):
     seconds = minutes * 60
-    while seconds > 0:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end='\r')
-        time.sleep(1)
-        seconds -= 1
-    print('Time is up!')
 
-focus_timer(25)  # 25分钟的专注时钟
+    def count_down():
+        nonlocal seconds
+        if seconds > 0:
+            mins, secs = divmod(seconds, 60)
+            timer_label.config(text='{:02d}:{:02d}'.format(mins, secs))
+            seconds -= 1
+            timer_label.after(1000, count_down)
+        else:
+            timer_label.config(text="Time's up!")
+
+    # 创建窗口
+    window = tk.Tk()
+    window.title("Focus Timer")
+
+    # 创建倒计时标签
+    timer_label = tk.Label(window, text='')
+    timer_label.pack()
+
+    # 启动倒计时
+    count_down()
+
+    # 运行窗口
+    window.mainloop()
+
+
+start_timer(25)  # 25分钟的专注时钟
 ```
